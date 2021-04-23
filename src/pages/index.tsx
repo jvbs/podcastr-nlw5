e8e9b1689481f87fbf7b9f480f2ee789883f9fc7
João Vitor Barbosa Souza <jvbs98@hotmail.com>
2021-04-23 11:11:56 -0300

    added getStaticProps and its types

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,39 @@
-export default function Home() {
-  return <h1>Hey</h1>;
+import { GetStaticProps } from "next"
+
+//? interface works the same aswell
+
+type Episode = {
+  id: string;
+  title: string;
+  members: string;
+}
+
+type HomeProps = {
+  episodes: Episode[];
+}
+
+/*
+type HomeProps = {
+  episodes: Array<{
+    id: string;
+    title: string;
+    members: string;
+  }>
+}
+*/
+
+export default function Home(props : HomeProps) {
+  return <p>{JSON.stringify(props.episodes)}</p>
+}
+
+export const getStaticProps : GetStaticProps = async () => {
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json()
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8 // 
+  }
 }
